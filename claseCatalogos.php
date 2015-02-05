9d4fc6d98470af783a052563cca2530c9601a092
guesslara <gerardolara1984@gmail.com>
2015-02-05 13:40:47 -0600

    correccion del path

--- claseCatalogos.php
+++ claseCatalogos.php
@@ -39,11 +39,11 @@ class catalogos{
    		$archivos=explode(",,,",$archivos);
    		$mensaje="";
    		for($i=0;$i<count($archivos);$i++){
-			$sql="SELECT CAT2_ARCHIVO.ID_ARCHIVO AS ID_ARCHIVO,PATH,ID_USUARIO_ASIGNADO FROM CAT2_ARCHIVO INNER JOIN CAT2_ARCHIVO_USUARIO ON CAT2_ARCHIVO.ID_ARCHIVO=CAT2_ARCHIVO_USUARIO.ID_ARCHIVO WHERE CAT2_ARCHIVO.ID_ARCHIVO='".$archivos[$i]."'";
+			$sql="SELECT CAT2_ARCHIVO.ID_ARCHIVO AS ID_ARCHIVO,PATH2,ID_USUARIO_ASIGNADO FROM CAT2_ARCHIVO INNER JOIN CAT2_ARCHIVO_USUARIO ON CAT2_ARCHIVO.ID_ARCHIVO=CAT2_ARCHIVO_USUARIO.ID_ARCHIVO WHERE CAT2_ARCHIVO.ID_ARCHIVO='".$archivos[$i]."'";
    			$res=$objDb->sqlQuery($sql);
 
    			while($row=$objDb->sqlFetchArray($res)){
-   				$path=$row["PATH"];
+   				$path=$row["PATH2"];
 				if($usuariosPrevios==""){
 	   				$usuariosPrevios=$row["ID_USUARIO_ASIGNADO"];
 	   			}else{
@@ -54,9 +54,7 @@ class catalogos{
 			if(unlink($path)){//se procede a eliminar el archivo
 				//se procede a eliminar el arciv
 	   			$sql="DELETE FROM CAT2_ARCHIVO_USUARIO WHERE ID_ARCHIVO='".$archivos[$i]."'";
-	   			
 	   			$res=$objDb->sqlQuery($sql);
-
 	   			if($res){
 	   				$sql1="DELETE FROM CAT2_ARCHIVO WHERE ID_ARCHIVO='".$archivos[$i]."'";
 	   				$res1=$objDb->sqlQuery($sql1);
